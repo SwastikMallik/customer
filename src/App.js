7516a42ab170f36ea5688b63e5e94cabe5268576
SwastikMallik <mallikswastik@gmail.com>
2024-09-26 21:20:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        Hello Swastik
+      </p>
+      <Routes>
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
